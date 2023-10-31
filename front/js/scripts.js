c2bd63d126d52fc3681c37945f7419a727b5ebc6
Rodrigo <23.00068-6@maua.br>
2023-10-31 08:36:18 -0300

    i

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -17,3 +17,25 @@ async function obterFilmes (){
         celulaSinopse.innerHTML = filme.sinopse;
     }
 }
+
+async function cadastrarFilme(){
+    const urlCompleta = `${protocolo}${host}${endPoint}`;
+    let tituloInput = document.querySelector('#tituloInput');
+    let sinopseInput = document.querySelector('#sinopseInput');
+    let titulo = tituloInput.value;
+    let sinopse = sinopseInput.value;
+    tituloInput.value = "";
+    sinopseInput.value ="";
+    const filmes = (await axios.post(urlCompleta, {titulo, sinopse})).data;
+
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    corpoTabela.innerHTML = "";
+    for (filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.titulo;
+        celulaSinopse.innerHTML = filme.sinopse;
+    }
+}
