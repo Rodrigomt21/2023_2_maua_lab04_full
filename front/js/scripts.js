66962dc54d7b2cc63ca9e4fe914829694d96c90a
Rodrigo <23.00068-6@maua.br>
2023-11-07 07:52:30 -0300

    yes

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -24,18 +24,31 @@ async function cadastrarFilme(){
     let sinopseInput = document.querySelector('#sinopseInput');
     let titulo = tituloInput.value;
     let sinopse = sinopseInput.value;
-    tituloInput.value = "";
-    sinopseInput.value ="";
-    const filmes = (await axios.post(urlCompleta, {titulo, sinopse})).data;
 
-    let tabela = document.querySelector('.filmes');
-    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
-    corpoTabela.innerHTML = "";
-    for (filme of filmes) {
-        let linha = corpoTabela.insertRow(0);
-        let celulaTitulo = linha.insertCell(0);
-        let celulaSinopse = linha.insertCell(1);
-        celulaTitulo.innerHTML = filme.titulo;
-        celulaSinopse.innerHTML = filme.sinopse;
+    if (titulo && sinopse){
+        tituloInput.value = "";
+        sinopseInput.value ="";
+        const filmes = (await axios.post(urlCompleta, {titulo, sinopse})).data;
+
+        let tabela = document.querySelector('.filmes');
+        let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+        corpoTabela.innerHTML = "";
+        for (filme of filmes) {
+            let linha = corpoTabela.insertRow(0);
+            let celulaTitulo = linha.insertCell(0);
+            let celulaSinopse = linha.insertCell(1);
+            celulaTitulo.innerHTML = filme.titulo;
+            celulaSinopse.innerHTML = filme.sinopse;
+        }
     }
+    else{
+        let alert = document.querySelector('.alert');
+        alert.classList.add('show');
+        alert.classList.remove('d-none');
+        setTimeout(() => {
+            alert.classList.add('d-none');
+            alert.classList.remove('show');
+        }, 2000)
+    }
+    
 }
